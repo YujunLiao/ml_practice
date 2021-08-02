01c946b3dffb48c51ec247d29e178dea3dbd4e70
YujunLiao <18916108830@163.com>
2021-08-02 14:54:04 +0800

    try to fix all the seed number, but fail.

--- cifar_demo.py
+++ cifar_demo.py
@@ -1,3 +1,5 @@
+import os
+
 import PIL
 import torch
 import torchvision
@@ -10,6 +12,20 @@ from collections import OrderedDict
 # from tensorboardX import SummaryWriter
 from torch.utils.tensorboard import SummaryWriter
 # from torch.utils.data.Dataset
+import random
+import numpy as np
+
+reproduce = True
+if reproduce:
+    print("deterministic")
+    torch.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
+    random.seed(0)
+    np.random.seed(0)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
+    os.environ['PYTHONHASHSEED'] = str(0)
 
 cifar10_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=None)
 cifar10_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=None)
@@ -28,11 +44,13 @@ class TensorDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
+
+
 cifar10_train_DL = DataLoader(
     TensorDataset(cifar10_trainset),
     batch_size=64,
     shuffle=False,
-    num_workers=0,
+    num_workers=1,
     collate_fn=None,
     pin_memory=False,
  )
@@ -41,13 +59,13 @@ cifar10_test_DL = DataLoader(
     TensorDataset(cifar10_testset),
     batch_size=1,
     shuffle=False,
-    num_workers=0,
+    num_workers=1,
     collate_fn=None,
     pin_memory=False,
  )
 
 resnet_18 = torchvision.models.resnet18(pretrained=True)
-alexnet = torchvision.models.alexnet(pretrained=True)
+alexnet = torchvision.models.alexnet(pretrained=False)
 
 class Model(Module):
     def __init__(self, model, out_num):
@@ -75,16 +93,18 @@ class Model(Module):
 
 # alex_model = Model(alexnet, 10)
 model = Model(alexnet, 10)
-# optim = torch.optim.SGD(model.parameters(), lr=0.0005, momentum=0.9)
-optim = torch.optim.SGD(model.parameters(), lr=0.0003, momentum=0.9)
+optim = torch.optim.SGD(model.parameters(), lr=0.0005, momentum=0.9)
+# optim = torch.optim.SGD(model.parameters(), lr=0.0003, momentum=0.9)
 # optim = torch.optim.SGD(model.parameters(), lr=0.0002, momentum=0.9)
 step_lr = torch.optim.lr_scheduler.StepLR(optim, step_size=30, gamma=0.1)
 
-device = torch.device('cuda:0')
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 writer = SummaryWriter(log_dir = 'logs')
+
 
 pre_loss = -1
 step = 0
+factor_n = 10
 for epoch in range(1):
     for i, (img, label) in enumerate(cifar10_train_DL):
         img, label, model = img.to(device), label.to(device), model.to(device)
@@ -102,10 +122,28 @@ for epoch in range(1):
         #             p['lr'] *= 0.5
         #             # print(p['lr'])
         # pre_loss = loss.item()
-        writer.add_scalar('loss', loss.item(), global_step=step)
+        writer.add_scalar('loss', round(loss.item(), factor_n), global_step=step)
         step += 1
-        print(f"epoch:{epoch} iter:{i} lr:{list(optim.param_groups)[0]['lr']} loss:{loss.item()}")
-    step_lr.step()
+        print(f"epoch:{epoch} iter:{i} lr:{list(optim.param_groups)[0]['lr']} loss:{round(loss.item(), factor_n)}")
+        step_lr.step()
+        if i == 100:
+            break
+
+model.eval()
+loss = 0
+n = 0
+acc = 0
+with torch.no_grad():
+    for i, (img, label) in enumerate(cifar10_test_DL):
+        img, label, model = img.to(device), label.to(device), model.to(device)
+        logit = model(img)
+        pre = torch.argmax(logit, dim=1)
+        acc += sum(pre==label)
+        loss += nn.CrossEntropyLoss()(logit, label)
+        n += 1
+        if n == 100:
+            break
+print(f"test loss:{round(loss.item()/n, factor_n)} acc:{acc/n}")
 
 
 
